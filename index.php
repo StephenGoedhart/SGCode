da0e636f9a7a1f23bd02278a1c0441e291dc1a9e
StephenGoedhart <sdg25@hotmail.com>
2017-07-10 15:46:20 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- CSS source -->
+    <link href="vendors/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="source/css/style.css" rel="stylesheet">
+    <!-- SEO metadata -->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="copyright" content="SGCode is a registered trademark of SGCode">
+    <meta name="description" content="Stephen Goedhart's portfolio website.">
+    <meta name="keywords" content="stephen, goedhart, portfolio, code, programming, design, sg, sgcode">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body>
+    <div class="container-fluid">
+      <header class="row">
+        <div class="hidden-xs col-sm-2 col-md-2 col-lg-2 height-sm-1 bg-warning"></div>
+        <div class="col-xs-3 col-sm-2 col-md-2 col-lg-3 height-xs-2 height-sm-1 height-md-1 height-lg-1">
+          <div class="logo"></div>
+        </div>
+        <!--<div class="col-xs-2 height-xs-1 bg-danger"> </div>-->
+      </header>
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 bg-primary">banner</div>
+    </div>
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="height:800px;">Portfolio</div>
+    </div>
+    <!-- javascript scripts -->
+    <script src="vendors/jQuery-3.2.1/jquery-3.2.1.min.js"></script>
+    <script src="vendors/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
