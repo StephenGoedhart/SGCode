63f85fc96f1bbeb48d4b15fa26e1081c04c8ed2d
StephenGoedhart <sdg25@hotmail.com>
2017-07-19 19:41:45 +0200

    all

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 <html>
   <head>
     <!-- CSS source -->
-    <link href="vendors/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendors/bootstrap-4.0.0-alpha.6-dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendors/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet">
+      <link href="vendors/resetCss/reset.css" rel="stylesheet">
     <link href="source/css/style.css" rel="stylesheet">
     <!-- SEO metadata -->
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -15,21 +17,50 @@
   </head>
   <body>
     <div class="container-fluid">
+
       <header class="row">
-        <div class="hidden-xs col-sm-2 col-md-2 col-lg-2 height-sm-1 bg-warning"></div>
-        <div class="col-xs-3 col-sm-2 col-md-2 col-lg-3 height-xs-2 height-sm-1 height-md-1 height-lg-1">
+        <div id="md-menu" class="col-sm-5 col-md-5 col-lg-5 height-md-1 bg-warning">
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">My work</a></li>
+            <li><a href="#">About</a></li>
+          </ul>
+        </div>
+        <div class="col-xs-3 col-sm-2 col-md-2 col-lg-2 height-xs-2">
           <div class="logo"></div>
         </div>
-        <!--<div class="col-xs-2 height-xs-1 bg-danger"> </div>-->
+        <div class="col-xs-2 height-xs-1 bg-danger"> </div>
       </header>
+
+      <div id="mobile-nav-main">
+        <div id="mobile-nav-element-index-1" class="mobile-nav-element text-center"><i class="fa fa-home"></i></div>
+        <div id="mobile-nav-element-index-2" class="mobile-nav-element text-center"><i class="fa fa-home"></i></div>
+        <div id="mobile-nav-element-index-3" class="mobile-nav-element text-center"><i class="fa fa-home"></i></div>
+      </div>
+
     <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 bg-primary">banner</div>
+        <div class="hidden-xs hidden-sm col-md-12 col-lg-12 height-md-1 height-lg-1"></div>
+        <div class="banner col-xs-12 col-sm-12 col-md-12 col-lg-12 bg-primary height-xs-4 height-sm-4 height-md-6 height-lg-6 height-xl-6">banner</div>
     </div>
+
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="height:800px;">Portfolio</div>
-    </div>
+    </div>]
+
+    <footer></footer>
     <!-- javascript scripts -->
+      <script>
+        Notification.requestPermission();
+        var o = new Notification("test");
+        setTimeout(function(){
+          if(Notification.permission == "granted"){
+            var o = new Notification("test", {
+              body: "this is the body"
+            });
+          }
+        },10000);
+      </script>
     <script src="vendors/jQuery-3.2.1/jquery-3.2.1.min.js"></script>
-    <script src="vendors/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+    <script src="vendors/bootstrap-4.0.0-alpha.6-dist/js/bootstrap.min.js"></script>
   </body>
 </html>
